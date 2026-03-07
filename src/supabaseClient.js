f7de2d5b5716ff3da0d2601e0007a8973f5fb2fb
Ben Krupp <ben.krupp@dat.com>
2026-03-06 18:54:09 -0700

    Refactor: split monolith into components, pages, lib

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://xrcgtkkaapfmzzjvyphu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyY2d0a2thYXBmbXp6anZ5cGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MTQwNTYsImV4cCI6MjA4ODM5MDA1Nn0.YilrzmLwST4A78UdwUj_FxCbOQ3W20soqVi-VwmyW7E';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
